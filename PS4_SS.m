6a6571c80919871794cd087c6753354aad1fd27d
chenxinhang12345 <1417798061@qq.com>
2016-07-19 00:19:28 +0800

    change

--- PS4_SS.m
+++ PS4_SS.m
@@ -16,8 +16,8 @@ injectedCurrent=15*dt; %The neuron's voltage will increase by 5mV/sec
 neuronVoltage(1)=restingV; %setting the first value to resting potential
 for i=2:length(t)
     neuronVoltage(i)=neuronVoltage(i-1)+injectedCurrent;
-    %over the course of 1 second (100 bc .01 dt)neurons voltage increas by
-    %5mV (which was divided /10O)
+    %over the course of 1 second (100 bc .01 dt)neurons voltage increase by
+    %5mV (which was divided /100)
     
     %The other part is we want this neuron to fire
     %Threshold SPIKE
@@ -39,8 +39,10 @@ clf
 plot(t,neuronVoltage)
 title('IF neuron without leak current ')
 %% Leak Current
+
+clear all
 dt=.01;  %increment
-t=0:dt:10;  %Time in seconds
+t=0:dt:100;  %Time in seconds
 
 neuronVoltage=zeros(size(t));  %Size of voltage vector (recorded in mV)= time
 restingV=-70; %-70 mV is resting potential 
@@ -49,26 +51,37 @@ threshold=-55; %-rrmV Where action potential stimulted
 %start by injecting current directly into the neuron and making its voltage
 %slowly go up 
 
-injectedCurrent=5*dt; %The neuron's voltage will increase by 5mV/sec
+injectedCurrent=15*dt; %The neuron's voltage will increase by 5mV/sec
 neuronVoltage(1)=restingV; %setting the first value to resting potential
-for i=2:length(t)
-    neuronVoltage(i)=neuronVoltage(i-1)+injectedCurrent;
-    %over the course of 1 second (100 bc .01 dt)neurons voltage increas by
-    %5mV (which was divided /10O)
+for i=1:length(t)-1%execute for time minus one 
+    if  neuronVoltage(i)<threshold && injectedCurrent>0;
+         
+    neuronVoltage(i+1)=neuronVoltage(i)+injectedCurrent;
+   injectedCurrent=15*dt;
+    end
+    %over the course of 1 second (100 bc .01 dt)neurons voltage increase by
+    %5mV (which was divided /100)
     
     %The other part is we want this neuron to fire
     %Threshold SPIKE
     
-    if neuronVoltage(i)>threshold && neuronVoltage(i)<50  %If reached threshold, make neuron spike
-        neuronVoltage(i)=50; %the spike (the overshoot) 
+    if neuronVoltage(i)>=threshold && injectedCurrent~=0  %If reached threshold, make neuron spike
+        neuronVoltage(i)=50;%the spike (the overshoot)
+        injectedCurrent=0;
         disp('spike')
     end
-    %Starts at resting membrane potential, slow increases until hit
-    %threshold, and then spike
-    
-    %Falling phase
-    if neuronVoltage(i) >50
-        neuronVoltage(i)= restingV;
+    if injectedCurrent==0
+        neuronVoltage(i+1)=neuronVoltage(i)+(restingV-neuronVoltage(i))*dt;
+   injectedCurrent=0;
     end
+    if neuronVoltage(i)<restingV && injectedCurrent==0
+        injectedCurrent=15*dt;
+        neuronVoltage(i+1)=neuronVoltage(i);
+        
+    end
+    
+   
+   
 end
-
+figure(2)
+plot(t,neuronVoltage)
